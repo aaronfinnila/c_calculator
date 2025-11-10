2df57b08352026a8cc42f79f2972757ff7286f67
aaronfinnila <aaronfinnilaa@gmail.com>
2025-11-10 22:51:40 +0200

    entry widget

--- example.c
+++ example.c
@@ -4,25 +4,22 @@ void print_num(GtkWidget *button, gpointer user_data) {
   const char *label = gtk_widget_get_name(button);
   int num = 0;
 
-  if (strcmp(label, "1") == 0) {
-    num = 1;
-  } else if (strcmp(label, "2") == 0) {
-    num = 2;
-  } else if (strcmp(label, "3") == 0) {
-    num = 3;
-  }
   g_print ("%d\n", num);
+}
+
+void react_click(GtkWidget *button, gpointer user_data) {
+  const gchar *usertext = gtk_editable_get_text(GTK_EDITABLE(user_data));
+  g_print("%s\n", usertext);
 }
 
 static void activate(GtkApplication *app) {
   GtkWidget *window;
-  GtkWidget *button1;
-  GtkWidget *button2;
-  GtkWidget *button3;
+  GtkWidget *button;
   GtkWidget *box;
+  GtkWidget *entry;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_title (GTK_WINDOW (window), "calc");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
   box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
@@ -31,22 +28,13 @@ static void activate(GtkApplication *app) {
 
   gtk_window_set_child (GTK_WINDOW (window), box);
 
-  button1 = gtk_button_new_with_label("1");
-  gtk_widget_set_name(button1, "1");
-  button2 = gtk_button_new_with_label("2");
-  gtk_widget_set_name(button2, "2");
-  button3 = gtk_button_new_with_label("3");
-  gtk_widget_set_name(button3, "3");
+  button = gtk_button_new_with_label("click to print");
+  entry = gtk_entry_new();
 
-  g_signal_connect (button1, "clicked", G_CALLBACK (print_num), NULL);
-  g_signal_connect (button2, "clicked", G_CALLBACK (print_num), NULL);
-  g_signal_connect (button3, "clicked", G_CALLBACK (print_num), NULL);
-/*   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window); */
+  g_signal_connect(button, "clicked", G_CALLBACK (react_click), entry);
 
-  gtk_box_append (GTK_BOX (box), button1);
-  gtk_box_append (GTK_BOX (box), button2);
-  gtk_box_append (GTK_BOX (box), button3);
-
+  gtk_box_append (GTK_BOX (box), button);
+  gtk_box_append (GTK_BOX (box), entry);
   gtk_window_present (GTK_WINDOW (window));
 }
 
